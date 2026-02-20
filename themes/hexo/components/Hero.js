020ce5416adb8d734c9d997dbdc2a366bb72595c
Wayne <wallensnow@gmail.com>
2026-02-20 19:05:06 +0800

    Update Hero.js

--- themes/hexo/components/Hero.js
+++ themes/hexo/components/Hero.js
@@ -60,7 +60,6 @@ const Hero = props => {
       id='header'
       style={{ zIndex: 1 }}
       className='w-full h-screen relative bg-black'>
-      // <div className='text-white absolute bottom-0 flex flex-col h-full items-center justify-center w-full '>
       <div
         className='text-white absolute bottom-0 flex flex-col h-full items-center justify-center w-full translate-y-8 md:translate-y-12'
         style={{
